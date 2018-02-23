85e664977b793a231b45302352984913f449fa07
Diane KL <dianekimlim@gmail.com>
2018-02-23 00:28:36 +0000

    chatty/server.js: added counter to keep track of connected users. Logs is internally counting this to later display

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -5,18 +5,31 @@ var uuidv4 = require('uuid/v4');
 
 const PORT = 3001
 
+let counter = 0;
+
 const server = express()
   .use(express.static('public'))
   .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`listening on ${PORT}`));
 
 const wss = new SocketServer({ server });
 
+	// Setup a broadcast helper
+	wss.broadcast = function broadcast(newMessage) {
+	  wss.clients.forEach(function each(client) {
+	    // only send the message to clients which are OPEN
+	    if (client.readyState === WebSocket.OPEN) {
+	      client.send(newMessage);
+	    }
+	  });
+	};
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
 wss.on('connection', (ws) => {
   console.log('Client connected');
-
+  counter += 1;
+  console.log(counter);
+  wss.broadcast(JSON.stringify({type: "Client connected", count: counter}));
   //Server handles incoming messages and user changes with notifications. 
   //This also creates a unique key for each user usind the UUID module.
   ws.on('message', function incoming(message) {
@@ -28,32 +41,18 @@ wss.on('connection', (ws) => {
   } else if (newMessage.type === "postNotification") {
   	newMessage.type === "incomingNotification"
   }
-  
 
-	// Setup a broadcast helper
-	wss.broadcast = newMessage => {
-	  wss.clients.forEach(client => {
-	    // only send the message to clients which are OPEN
-	    if (client.readyState === WebSocket.OPEN) {
-	      client.send(JSON.stringify(newMessage));
-	    }
-	  });
-	};
-	// });
     // When we get a message from a client, re-broadcast it to all connected clients
-    wss.broadcast(newMessage);
+    wss.broadcast(JSON.stringify(newMessage));
   });
   
-  // wss.clients.forEach(function each(client) {
-  // 	if (client.readyState === SocketServer.OPEN) {
-  // 		client.send(JSON.stringify(newMessage))
-  // 	}
-  // })
 
-  // Send an initial message to clients on connection.
-  ws.send(JSON.stringify({ username: "Bot", message: "Welcome to the Chatty room :)" }));
-
-//   ws.on('close', () => console.log('Client disconnected'));
+  ws.on('close', () => {
+  	console.log('Client disconnected')
+  	counter -= 1;
+  	console.log(counter);
+  	wss.broadcast(JSON.stringify({type: "Client disconnected", count: counter}));
+  });
 });
 
 
